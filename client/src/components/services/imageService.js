50219d8cf9a8dd1986f371a3bb36d732ce6d88d6
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2021-02-28 20:52:19 +0200

    2021/02/28 20:47

--- client/src/components/services/imageService.js
+++ client/src/components/services/imageService.js
@@ -2,7 +2,7 @@ import http from "./httpService";
 import { apiUrl } from "../../config.json";
 
 export async function uploadImage(data){
-  return http.post(`${apiUrl}/images`, data).catch( err => console.log(err));
+  return http.post(`${apiUrl}/images`, data).then(res => { console.log(res.data); }).catch( err => console.log(err));
 }
 
 export async function getImage(){
